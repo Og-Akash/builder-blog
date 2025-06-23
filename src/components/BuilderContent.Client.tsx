975c525b53332da858e17c8434b487b7c14efcdb
Akash Ghosh <akashghoshofficial700@gmail.com>
2025-06-23 12:57:18 +0530

    fix: update content prop type to unknown for better type safety; add early return for invalid content

--- src/components/BuilderContent.Client.tsx
+++ src/components/BuilderContent.Client.tsx
@@ -3,12 +3,13 @@
 import { BuilderContent } from "@builder.io/react";
 import { RenderBuilderContent } from "@/components/builder";
 
-
 interface Props {
-  content: any;
+  content: unknown;
 }
 
 export default function BlogContent({ content }: Props) {
+  if (!content || typeof content !== "object") return null;
+  
   return (
     <BuilderContent model="blog" content={content}>
       {(data, loading, fullContent) => (
